22e7c6bb5ec82af8d060b1ffcea08baac450abcb
Lukasz Piotr <lukaszmordal@wp.pl>
2018-05-03 22:31:21 +0200

    Testy

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        struct TestowaStruktura
+        {
+            public int x;
+            public int y;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            Console.WriteLine(mnozenie(8));
+        }
+        static int mnozenie(int zmienna1)
+        {
+            return zmienna1 * 10;
+
+        }
+    }
+}
